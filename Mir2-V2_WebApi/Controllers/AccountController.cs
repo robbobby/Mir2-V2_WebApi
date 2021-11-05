b3442bb1e35cbbe45cc6e86f640f15189a6534af
robbobby <69080884+robbobby@users.noreply.github.com>
2021-11-05 16:01:11 +0000

    Web service implemented on client

--- Mir2-V2_WebApi/Controllers/AccountController.cs
+++ Mir2-V2_WebApi/Controllers/AccountController.cs
@@ -4,17 +4,17 @@ using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Models_Mir2_V2_WebApi;
+using Models_Mir2_V2_WebApi.Enums;
 using Models_Mir2_V2_WebApi.Model;
 using Serilog;
 using SharedModels_Mir2_V2.AccountDto;
 namespace Mir2_v2_WebApi.Controllers {
 
     [ApiController]
-    [Route("[controller]")]
-    public class AccountController {
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class AccountController : ControllerBase {
 
         private readonly IDataAccessService<AccountDbEntry> accountDataAccessService;
 
@@ -23,13 +23,13 @@ namespace Mir2_v2_WebApi.Controllers {
             accountDataAccessService = _accountDataAccessService;
         }
 
-        [HttpGet("[action]")]
+        [Microsoft.AspNetCore.Mvc.HttpGet("[action]")]
         public async Task<IEnumerable<AccountDbEntry>> GetAllAccounts(int _accountId = 1) {
             return await accountDataAccessService.GetAllAccounts();
         }
 
 
-        [HttpGet("[action]")]
+        [Microsoft.AspNetCore.Mvc.HttpGet("[action]")]
         public async Task<AccountDbEntry> GetAccount(int _accountId = 1) {
             AccountDbEntry x = await accountDataAccessService.GetAccount(_accountId);
             Log.Error(x.Email);
@@ -37,12 +37,15 @@ namespace Mir2_v2_WebApi.Controllers {
             return x;
         }
 
-        [HttpPost("[action]")]
-        public async Task RegisterNewAccount([FromBody] AccountRegisterDtoC2S _accountDbEntry) {
+        [Microsoft.AspNetCore.Mvc.HttpPost("[action]")]
+        public async Task<IActionResult> RegisterNewAccount([Microsoft.AspNetCore.Mvc.FromBody] AccountRegisterDtoC2S _accountDbEntry) {
             HttpResponseMessage responseMessage = new HttpResponseMessage();
             responseMessage.StatusCode = HttpStatusCode.InternalServerError;
-            if  (! new EmailAddressAttribute().IsValid(_accountDbEntry.Email))
-                return;
+            if (!new EmailAddressAttribute().IsValid(_accountDbEntry.Email))
+                return UnprocessableEntity(AccountError.EmailNotValid);
+            if (accountDataAccessService.IsEmailAlreadyRegistered(_accountDbEntry.Email))
+                return UnprocessableEntity(AccountError.EmailAlreadyInUse);
+            
             AccountDbEntry account = new AccountDbEntry() {
                 Email = _accountDbEntry.Email,
                 FirstName = _accountDbEntry.FirstName,
@@ -52,11 +55,12 @@ namespace Mir2_v2_WebApi.Controllers {
                 SessionToken = Guid.Empty,
                 IsLoggedIn = false
             };
-            AccountDbEntry accountResponse = await accountDataAccessService.PostAccount(account);
+            var accountResponse = await accountDataAccessService.PostAccount(account);
+            return Ok();
         }
 
 
-        [HttpPost("[action]")]
+        [Microsoft.AspNetCore.Mvc.HttpPost("[action]")]
         public async Task DeleteAccount(int _accountId) {
             accountDataAccessService.DeleteAccount(_accountId);
         }
---
+++ SharedModels_Mir2-V2/Enums/AccountError.cs
@@ -0,0 +1,6 @@
+namespace Models_Mir2_V2_WebApi.Enums {
+    public enum AccountError {
+        EmailNotValid,
+        EmailAlreadyInUse
+    }
+}
