287d2a5c730badaa9f4ce99c7d92f9b5315e8a1a
robbobby <69080884+robbobby@users.noreply.github.com>
2021-11-09 08:23:04 +0000

    Updated nuget from shared models, and altered script to just import dll file into Unity

--- SharedModels_Mir2-V2/UnityBuild.sh
+++ SharedModels_Mir2-V2/UnityBuild.sh
@@ -4,7 +4,8 @@ dotnet build
 
 echo "Removing old dll and metal files"
 rm -rf ../../../MMORPG/Assets/Models_Mir2-V2_WebApi
+mkdir ../../../MMORPG/Assets/Models_Mir2-V2_WebApi
 
 echo "Adding new dll files"
-cp -rf bin/Debug ../../../MMORPG/Assets/Models_Mir2-V2_WebApi
+cp bin/Debug/SharedModels_Mir2_V2.dll ../../../MMORPG/Assets/Models_Mir2-V2_WebApi/
 
